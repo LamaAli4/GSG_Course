0d2ae787d4774950ff6918582654ffce538c9dbd
LamaAli4 <lama2015678@gmail.com>
2024-12-28 10:02:07 +0200

    to do app exercise

--- To Do List/src/App.tsx
+++ To Do List/src/App.tsx
@@ -1,18 +1,46 @@
-// import { useState } from 'react'
-import './App.css'
-import AllTodos from './components/AllToDos'
-import Form from './components/Form'
+import { useState } from "react";
+import "./App.css";
+import Dashboard from "./components/dashboard/dashboard.component";
+import Form from "./components/form/form.components";
+import TodoList from "./components/todo-list/todo-list.component";
+import { ITodoItem } from "./components/types";
 
 function App() {
+  const [todos, setTodos] = useState<ITodoItem[]>([]);
 
+  const handleNewItem = (item: ITodoItem) => {
+    setTodos([...todos, item]);
+  };
+
+  const handleTaskToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const itemId = e.target.dataset["itemId"];
+    setTodos(
+      todos.map((item) =>
+        item.id === Number(itemId) ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
+
+  const handleDelete = (index: number) => {
+    // This will delete the item at index!
+    setTodos([
+      ...todos.slice(0, index),
+      ...todos.slice(index + 1, todos.length),
+    ]);
+  };
 
   return (
     <div>
-         <h1>To Do List</h1>
-         <Form/>
-         <AllTodos/>
+      <h1>Todo App - {new Date().toDateString()}</h1>
+      <Form onSubmit={handleNewItem} />
+      <Dashboard items={todos} />
+      <TodoList
+        items={todos}
+        onToggle={handleTaskToggle}
+        onDelete={handleDelete}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
