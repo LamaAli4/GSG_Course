4713c82859707ecd0230132287a4cab466a8a642
LamaAli4 <lama2015678@gmail.com>
2025-01-23 23:14:04 +0200

    Handling new student (Student App 6)

--- Student App/src/App.tsx
+++ Student App/src/App.tsx
@@ -5,7 +5,6 @@ import { IStudent } from "./types";
 import Student from "./assets/components/student/student.component";
 import AddForm from "./assets/components/add-form/add-form.component";
 
-
 const COURSES_LIST = ["React", "HTML", "CSS"];
 const INITIAL_LIST: Array<IStudent> = [
   {
@@ -27,60 +26,58 @@ const INITIAL_LIST: Array<IStudent> = [
     name: "Waleed Fadi",
     age: 23,
     isGraduated: true,
-    coursesList: COURSES_LIST, 
+    coursesList: COURSES_LIST,
   },
   {
     id: "2404",
     name: "Sarah Waheed",
     age: 19,
     isGraduated: true,
-    coursesList: COURSES_LIST, 
+    coursesList: COURSES_LIST,
   },
   {
     id: "2405",
     name: "Mohammed Ahmad",
-    age: 22, 
+    age: 22,
     isGraduated: true,
-    coursesList: COURSES_LIST, 
+    coursesList: COURSES_LIST,
   },
 ];
 
-
 function App() {
-  const [studentsList, setStudentsList] = useState <IStudent[]>(INITIAL_LIST);
-  const [totalAbsents,setTotalAbsents] = useState(0)
+  const [studentsList, setStudentsList] = useState<IStudent[]>(INITIAL_LIST);
+  const [totalAbsents, setTotalAbsents] = useState(0);
 
   const removeLast = () => {
-    const newList = [...studentsList]
+    const newList = [...studentsList];
     newList.shift();
     setStudentsList(newList);
-  }
+  };
 
   const handleAbsentChange = (name: string, change: number) => {
-    console.log ("[App.tsx] Absent Changed")
-    console.log (`${name}: ${change}`); 
-    setTotalAbsents (totalAbsents + change);   
-  }
+    setTotalAbsents(totalAbsents + change);
+  };
+
+  const handleAddStudent = (newStudent: IStudent) => {
+    setStudentsList([newStudent, ...studentsList]);
+  };
   return (
     <div>
       <h1 style={{ color: "#a3ff55" }}>Welcome to GSG React/Next Course</h1>
-      <AddForm/>
+      <AddForm onSubmit={handleAddStudent} />
       <button onClick={removeLast}>Remove Last Student</button>
       <b>Total Absents {totalAbsents}</b>
-      {
-        studentsList.map(student=> (      
-          <Student
-            key={student.id}
-            id= {student.id}
-            name= {student.name}
-            age= {student.age}
-            isGraduated={student.isGraduated}
-            coursesList={student.coursesList}
-            onAbsentChange={handleAbsentChange}
-          /> 
-        )
-        )
-      }
+      {studentsList.map((student) => (
+        <Student
+          key={student.id}
+          id={student.id}
+          name={student.name}
+          age={student.age}
+          isGraduated={student.isGraduated}
+          coursesList={student.coursesList}
+          onAbsentChange={handleAbsentChange}
+        />
+      ))}
     </div>
   );
 }
